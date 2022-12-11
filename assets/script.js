1b28bde049afafdbc57e48e9cc99e5902937a2a1
William Kerby <w.kerby99@gmail.com>
2022-12-11 18:55:46 -0500

    Create function in script.js file

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,32 @@
-var topics = ["HTML","CSS","Git","JavaScript"]
-for (var x = 0; x<topics.length; x++)
-{if (topics[x] === "HTML") 
-    console.log("HTML");
- else if (topics[x] === "CSS") 
-    console.log("CSS");
- else if (topics[x] === "Git") 
-    console.log("Git");
- else if (topics[x] === "JavaScript") 
-    console.log("JavaScript");
- else 
-    console.log("Please try again!");
+// define the randomTopic and topics variables
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+
+//create function to list topics in the course
+function listTopics() {
+ for (var x = 0; x < topics.length; x++) {
+   console.log(topics[x]);
+ }
 }
+
+//create function to select topics in the course
+function selectTopic() {
+ if (randomTopic === 'HTML') {
+   console.log("Let's study HTML!");
+ } else if (randomTopic === 'CSS') {
+   console.log("Let's study CSS!");
+ } else if (randomTopic === 'Git') {
+   console.log("Let's study Git!");
+ } else if (randomTopic === 'JavaScript') {
+   console.log("Let's study JavaScript!");
+ } else {
+   console.log('Please try again!');
+ }
+}
+
+console.log("Here are the topics that we learned through Prework:");
+listTopics();
+
+console.log("Which topic should we study first?");
+selectTopic();
